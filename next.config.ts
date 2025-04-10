2dbec6a8e0de0113975bddc4a11c88de67732e22
MrPhyaeSoneThwim <mr.phyaesonethwim1998@gmail.com>
2025-04-11 00:59:57 +0700

    added necessary config to solve build issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  output: 'standalone',
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
